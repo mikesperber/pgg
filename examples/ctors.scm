72a9c4ff476a9488e501bc4e7b4f1155339d4582
thiemann <none@none>
1995-10-09 13:19:11 +0200

    An example with partially static data.

---
+++ examples/ctors.scm
@@ -0,0 +1,17 @@
+(defdata my-list (my-nil) (my-cons my-car my-cdr)) 
+(define (main ss dd)
+  (my-length (list->my-list-by ss dd)))
+(define (list->my-list-by ss dd)
+  (cond
+   ((null? ss)
+    (my-nil))
+   ((= 1 (length ss))
+    (my-cons (car dd)
+	     (my-nil)))
+   (else
+    (my-cons (car dd)
+	     (list->my-list-by (cdr ss) (cdr dd))))))
+(define (my-length l)
+  (if (my-nil? l)
+      0
+      (+ 1 (my-length (my-cdr l)))))
