7f1aab6e1defe355dabbede9721fa3ba5243aab7
thiemann <none@none>
1997-04-25 15:11:39 +0200

    runs

--- examples/pm.scm
+++ examples/pm.scm
@@ -3,53 +3,46 @@
 (define (match p d) ;;; Pat x Dat -> Ans
   ((match-hook p d cons) '() '()))
 
-(define   ;;; Pat x Dat x Cont -> Cont
-  (match-hook p d k)
-    ((case (car p)
-      ((Cst) (let ((v (cadr p))) (if (equal? v d) k fail)))
-      ((Var) (let ((n (cadr p))) (assoc-c n (test-and-jump d k) (extend n d k))))
-      ((Seq) (let ((p* (cdr p))) (match-seq p* d k))))))
+(define (match-hook p d k)   ;;; Pat x Dat x Cont -> Cont
+  (case (car p)
+    ((Cst) (let ((v (cadr p))) (if (equal? v d) k fail)))
+    ((Var) (let ((n (cadr p))) (assoc-c n (test-and-jump d k) (extend n d k))))
+    ((Seq) (let ((p* (cdr p))) (match-seq p* d k)))))
 
-(define   ;;; Pat* x Dat x Cont -> Cont
-  (match-seq p* d k)
-    ((if (null? p*)
-	(if (null? d) k fail)
-	(let ((p (car p*)) (p* (cdr p*)))
-	  (if (pair? d)
-	      (match-hook p (car d) (match-seq p* (cdr d) k))
-	      fail)))))
+(define (match-seq p* d k)   ;;; Pat* x Dat x Cont -> Cont
+  (if (null? p*)
+      (if (null? d) k fail)
+      (let ((p (car p*)) (p* (cdr p*)))
+	(if (pair? d)
+	    (match-hook p (car d) (match-seq p* (cdr d) k))
+	    fail))))
 
-(define  ;;; Cont = (List(Nam) x List(Val) -> Ans)
-  (fail ln lv)
-    (#f))
+(define (fail ln lv)  ;;; Cont = (List(Nam) x List(Val) -> Ans)
+  #f)
 
-(define  ;;; Val x Cont -> Val -> Cont
-  (test-and-jump d k)
-    ((lambda (v)
-      (if (equal? d v)
-	  k
-	  fail))))
+(define (test-and-jump d k)  ;;; Val x Cont -> Val -> Cont
+  (lambda (v)
+    (if (equal? d v)
+	k
+	fail)))
 
-(define  ;;; Nam x Val x Cont -> Cont
-  (extend n d k)
-    ((lambda (ln lv)
-      (k (cons n ln) (cons d lv)))))
+(define (extend n d k)  ;;; Nam x Val x Cont -> Cont
+  (lambda (ln lv)
+    (k (cons n ln) (cons d lv))))
 
-(define  ;;; Nam x (Val -> Cont) x Cont -> Cont
-  (assoc-c n s f)
-    ((lambda (ln lv)
-      (let ((offset (index n ln)))
-	(if (negative? offset)
-	    (f ln lv)
-	    ((s (list-ref lv offset)) ln lv))))))
+(define (assoc-c n s f)  ;;; Nam x (Val -> Cont) x Cont -> Cont
+  (lambda (ln lv)
+    (let ((offset (index n ln)))
+      (if (negative? offset)
+	  (f ln lv)
+	  ((s (list-ref lv offset)) ln lv)))))
 
-(define  ;;; Nam x List(Nam) -> Nat + {-1}
-  (index e l)
-    (((let loop ((n 0) (l l)
-		 (cond
-		  ((null? l)
-		   -1)
-		  ((equal? e (car l))
-		   n)
-		  (else
-		   (loop (add1 n) (cdr l)))))))))
+(define (index e l)  ;;; Nam x List(Nam) -> Nat + {-1}
+  (let loop ((n 0) (l l))
+    (cond
+     ((null? l)
+      -1)
+     ((equal? e (car l))
+      n)
+     (else
+      (loop (+ 1 n) (cdr l))))))
