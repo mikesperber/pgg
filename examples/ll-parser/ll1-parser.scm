d441f108787d37f683d385625428061260b2f53d
thiemann <none@none>
1998-07-27 10:27:16 +0200

    changes in the ll-parser

--- examples/ll-parser/ll1-parser.scm
+++ examples/ll-parser/ll1-parser.scm
@@ -1,47 +1,52 @@
 ;; LL (1) parser
 
 (define-memo _memo 1)
+(define-primitive eq? - pure)
+(define-primitive cdr - pure)
+(define-primitive memv - pure)
 ;; (define-primitive apply - apply)
 
 (define (parse grammar input)
-  (let ((first-map (compute-first grammar 1)))
 
-    (define-without-memoization (parse-one symbol input)
-      (let ((ch (car input)))
-	(if (terminal? symbol grammar)
-	    (if (eq? (grammar-symbol->name symbol grammar) ch)
-		(values ch (cdr input))
-		(error "parse error, expected: " symbol " got: " ch))
-	    (let ((try
-		   (lambda (production)
-		     (call-with-values
-		      (lambda ()
-			(parse-sequence (production-rhs production) input))
-		      (lambda (val* input)
-			(values (apply (eval (production-attribution production)
-					     (interaction-environment))
-				       val*)
-				input))))))
-	      (let loop ((productions (grammar-productions-with-lhs symbol grammar)))
-		(cond
-		 ((null? productions)
-		  (error "parse error, unexpected symbol" ch))
-		 ((= 1 (length productions))
-		  (try (car productions)))
-		 ((memq ch (first-map symbol))
-		  (try (car productions)))
-		 (else
-		  (loop (cdr productions)))))))))
+  (define-without-memoization (parse-one symbol input)
+    (_memo
+     (let ((ch (car input)))
+       (if (terminal? symbol grammar)
+	   (if (eq? (grammar-symbol->name symbol grammar) ch)
+	       (values ch (cdr input))
+	       (error "parse error, expected: " symbol " got: " ch))
+	   (let ((try
+		  (lambda (production)
+		    (call-with-values
+		     (lambda ()
+		       (parse-sequence (production-rhs production) input))
+		     (lambda (val* input)
+		       (values (apply (eval (production-attribution production)
+					    (interaction-environment))
+				      val*)
+			       input))))))
+	     (let loop ((productions (filter (lambda (production)
+					       (eq? symbol (production-lhs production)))
+					     (grammar-productions grammar))))
+	       (cond
+		((null? productions)
+		 (error "parse error, unexpected symbol" ch))
+		((= 1 (length productions))
+		 (try (car productions)))
+		((memv ch (map (lambda (x) (car x))
+			       (nonterminal-first symbol 1 grammar)))
+		 (try (car productions)))
+		(else
+		 (loop (cdr productions))))))))))
 
-    (define-without-memoization (parse-sequence symbol* input)
-      (_memo
-       (let loop ((symbol* symbol*) (val* '()) (input input))
-	 (if (null? symbol*)
-	     (values (reverse val*) input)
-	     (call-with-values
-	      (lambda ()
-		(parse-one (car symbol*) input))
-	      (lambda (val input)
-		(loop (cdr symbol*) (cons val val*) input)))))))
+  (define-without-memoization (parse-sequence symbol* input)
+    (let loop ((symbol* symbol*) (val* '()) (input input))
+      (if (null? symbol*)
+	  (values (reverse val*) input)
+	  (call-with-values
+	   (lambda ()
+	     (parse-one (car symbol*) input))
+	   (lambda (val input)
+	     (loop (cdr symbol*) (cons val val*) input))))))
 
-    (parse-one (grammar-start grammar) input)))
+  (parse-one (grammar-start grammar) input))
--- examples/ll-parser/packages.scm
+++ examples/ll-parser/packages.scm
@@ -15,7 +15,7 @@
 
 	  nonterminal-nullable? sequence-nullable?
 	  nonterminal-first sequence-first
-	  nonterminal-follow compute-first))
+	  nonterminal-follow))
 
 (define-interface scc-union-interface
   (export complete-subsets!))
@@ -23,12 +23,19 @@
 (define-interface parser-interface
   (export parse))
 
+(define-interface toy-grammars-interface
+  (export g10 (g10-symbol :syntax) i10-1 i10-2 i10-3))
+
 ; Structures
 
 (define-structure grammar grammar-interface
   (open scheme big-util defrecord enumerated scc-union)
-  (files (grammar)))
+  (files grammar))
 
 (define-structure scc-union scc-union-interface
   (open scheme)
-  (files (scc-union)))
+  (files scc-union))
+
+(define-structure toy-grammars toy-grammars-interface
+  (open scheme enumerated grammar)
+  (files toy-grammars))
